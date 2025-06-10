3e184415283d4b67969e0b694cdaaf1c739db0ac
kimyongjun0129 <kimyongjun0129@naver.com>
2025-06-10 23:38:41 +0900

    레벨 4 : Interceptor로 로깅 구현

---
+++ src/main/java/org/example/expert/config/CustomInterceptor.java
@@ -0,0 +1,29 @@
+package org.example.expert.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
+public class CustomInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if (request.getRequestURI().startsWith("/admin")) {
+            Date today = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            log.info("인증 성공 시간 = {}", dateFormat.format(today));
+            log.info("요청 URL = {}", request.getRequestURL());
+        }
+
+        return true;
+    }
+}
