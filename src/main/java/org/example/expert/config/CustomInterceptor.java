be922d03525b8bbac6d96063a5107d6fc0de90b4
kimyongjun0129 <kimyongjun0129@naver.com>
2025-06-12 13:10:05 +0900

    레벨 4 : Interceptor 주석 해제:

--- src/main/java/org/example/expert/config/CustomInterceptor.java
+++ src/main/java/org/example/expert/config/CustomInterceptor.java
@@ -13,17 +13,17 @@ import java.util.Date;
 @Slf4j
 public class CustomInterceptor implements HandlerInterceptor {
 
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//
-//        if (request.getRequestURI().startsWith("/admin")) {
-//            Date today = new Date();
-//            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//
-//            log.info("인증 성공 시간 = {}", dateFormat.format(today));
-//            log.info("요청 URL = {}", request.getRequestURL());
-//        }
-//
-//        return true;
-//    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if (request.getRequestURI().startsWith("/admin")) {
+            Date today = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            log.info("인증 성공 시간 = {}", dateFormat.format(today));
+            log.info("요청 URL = {}", request.getRequestURL());
+        }
+
+        return true;
+    }
 }
